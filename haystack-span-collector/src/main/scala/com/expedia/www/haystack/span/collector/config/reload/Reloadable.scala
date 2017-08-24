3d3de9fd8b186d306bb10f1e05e4bea84fe627b7
Ashish Aggarwal <asaggarwal@DELC02T84M5G8WN.sea.corp.expecn.com>
2017-08-24 16:44:01 +0530

    Renaming colletor from stitched to buffered spans

---
+++ haystack-span-collector/src/main/scala/com/expedia/www/haystack/span/collector/config/reload/Reloadable.scala
@@ -0,0 +1,31 @@
+/*
+ *  Copyright 2017 Expedia, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+
+package com.expedia.www.haystack.span.collector.config.reload
+
+/**
+  * An entity(for e.g. a configuration object) that extends reloadable trait allows it to reload the config object
+  * dynamically. The config reloader reads the new configuration periodically from an external store and
+  * calls 'onReload()' method.
+  * The 'name' provides the tableName where the configuration is stored for this entity.
+  * see [[com.expedia.www.haystack.span.collector.config.reload.ConfigurationReloadProvider]]
+  */
+trait Reloadable {
+  def name: String
+
+  def onReload(newConfig: String): Unit
+}

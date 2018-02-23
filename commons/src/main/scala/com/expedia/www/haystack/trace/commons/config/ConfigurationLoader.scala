6a44f1ac1492f4964563fa68dee950cd036069da
Ashish Aggarwal <asaggarwal@DELC02T84M5G8WN.sea.corp.expecn.com>
2018-02-24 00:00:02 +0530

    allow array looking env variable values into iterable hocon objects

--- commons/src/main/scala/com/expedia/www/haystack/trace/commons/config/ConfigurationLoader.scala
+++ commons/src/main/scala/com/expedia/www/haystack/trace/commons/config/ConfigurationLoader.scala
@@ -20,6 +20,8 @@ package com.expedia.www.haystack.trace.commons.config
 import java.io.File
 
 import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.commons.lang3.StringUtils
+
 import scala.collection.JavaConversions._
 
 object ConfigurationLoader {
@@ -45,10 +47,10 @@ object ConfigurationLoader {
     * @return new config object with haystack specific environment variables
     */
   private def loadFromEnvVars(): Config = {
-    val envMap = sys.env.filter {
+    val envMap: Map[String, Object] = sys.env.filter {
       case (envName, _) => isHaystackEnvVar(envName)
     } map {
-      case (envName, envValue) => (transformEnvVarName(envName), envValue)
+      case (envName, envValue) => (transformEnvVarName(envName), transformEnvVarValue(envValue))
     }
 
     ConfigFactory.parseMap(envMap)
@@ -60,9 +62,23 @@ object ConfigurationLoader {
     * converts the env variable to HOCON format
     * for e.g. env variable HAYSTACK_KAFKA_STREAMS_NUM_STREAM_THREADS gets converted to kafka.streams.num.stream.threads
     * @param env environment variable name
-    * @return
+    * @return variable name that complies with hocon key
     */
   private def transformEnvVarName(env: String): String = {
     env.replaceFirst(ENV_NAME_PREFIX, "").toLowerCase.replace("_", ".")
+  }
+
+  /**
+    * converts the env variable value to iterable object if it starts and ends with '[' and ']' respectively.
+    * @param env environment variable value
+    * @return string or iterable object
+    */
+  private def transformEnvVarValue(env: String): Object = {
+    if (env.startsWith("[") && env.endsWith("]")) {
+      import scala.collection.JavaConverters._
+      env.substring(1, env.length - 1).split(',').filter(StringUtils.isNotEmpty).toList.asJava
+    } else {
+      env
+    }
   }
 }

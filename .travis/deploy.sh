dc55da2d4e0976d8553a8e36653de0815d2c9ab8
krastogi <krastogi@expedia.com>
2018-07-04 15:09:35 +0530

    Passing skipGpg property to pom

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -33,7 +33,7 @@ else
     GPG_SKIP=true
 fi
 
-mvn deploy --settings .travis/settings.xml -DskipGpg=${GPG_SKIP} -DskipTests=true -B -U
+mvn clean deploy --settings .travis/settings.xml -DskipGpg=${GPG_SKIP} -DskipTests=true -B -U
 SUCCESS=$?
 
 if [ ${SUCCESS} -eq 0 ]

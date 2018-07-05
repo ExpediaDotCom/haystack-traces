9a24f1cf7b6f58cea334ee2cfc389398c1d520f5
Kevin Tholen <ktholen@homeaway.com>
2018-07-05 15:58:05 -0500

    Typo fix and add copyright

--- reader/src/test/scala/com/expedia/www/haystack/trace/reader/unit/stores/readers/es/query/TraceSearchQueryGeneratorSpec.scala
+++ reader/src/test/scala/com/expedia/www/haystack/trace/reader/unit/stores/readers/es/query/TraceSearchQueryGeneratorSpec.scala
@@ -29,7 +29,7 @@ import org.scalatest.BeforeAndAfterEach
 class TraceSearchQueryGeneratorSpec extends BaseUnitTestSpec with BeforeAndAfterEach {
   private val esConfig = ElasticSearchConfiguration("endpoint", None, None, "haystack-traces", "spans", 5000, 5000, 6, 72, false)
 
-  var timezone = "UCT"
+  var timezone = "UTC"
 
   override def beforeEach() {
     timezone = System.getProperty("user.timezone")

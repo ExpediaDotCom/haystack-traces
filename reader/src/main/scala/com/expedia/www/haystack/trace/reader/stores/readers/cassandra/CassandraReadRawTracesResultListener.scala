5f49528846a20d115f79b5942d0f90a4904045f0
krastogi <krastogi@expedia.com>
2018-08-07 12:00:52 +0530

    Added endpoint for getting traces details for given traceIds

--- reader/src/main/scala/com/expedia/www/haystack/trace/reader/stores/readers/cassandra/CassandraReadRawTracesResultListener.scala
+++ reader/src/main/scala/com/expedia/www/haystack/trace/reader/stores/readers/cassandra/CassandraReadRawTracesResultListener.scala
@@ -79,8 +79,8 @@ class CassandraReadRawTracesResultListener(asyncResult: ResultSetFuture,
         case Success(sBuffer) =>
           tracesMap.get(sBuffer.getTraceId) match {
             case Some(trace) =>
-              val updatedTrace = trace.toBuilder.addAllChildSpans(trace.getChildSpansList).build()
-              tracesMap.put(sBuffer.getTraceId, updatedTrace)
+              val updatedTrace = trace.toBuilder.addAllChildSpans(sBuffer.getChildSpansList).build()
+              tracesMap.update(sBuffer.getTraceId, updatedTrace)
             case _ =>
               tracesMap.put(sBuffer.getTraceId, traceBuilder.setTraceId(sBuffer.getTraceId).addAllChildSpans(sBuffer.getChildSpansList).build())
           }
